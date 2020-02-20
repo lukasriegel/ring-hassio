83b59d4a3cc1a0c851cf15e2f4a59dd8ac16f169
Riegel, Lukas <lukas.riegel@sap.com>
2020-02-20 09:41:10 +0100

    test

--- ring_hassio/livestream.js
+++ ring_hassio/livestream.js
@@ -1,46 +1,129 @@
 "use strict";
 //Far majority of this code by Dgreif https://github.com/dgreif/ring/examples/browser_example.ts
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+    function adopt(value) {
+        return value instanceof P ? value : new P(function (resolve) {
+            resolve(value);
+        });
+    }
+    return new(P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) {
+            try {
+                step(generator.next(value));
+            } catch (e) {
+                reject(e);
+            }
+        }
+
+        function rejected(value) {
+            try {
+                step(generator["throw"](value));
+            } catch (e) {
+                reject(e);
+            }
+        }
+
+        function step(result) {
+            result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
+        }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
 var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
+    var _ = {
+            label: 0,
+            sent: function () {
+                if (t[0] & 1) throw t[1];
+                return t[1];
+            },
+            trys: [],
+            ops: []
+        },
+        f, y, t, g;
+    return g = {
+        next: verb(0),
+        "throw": verb(1),
+        "return": verb(2)
+    }, typeof Symbol === "function" && (g[Symbol.iterator] = function () {
+        return this;
+    }), g;
+
+    function verb(n) {
+        return function (v) {
+            return step([n, v]);
+        };
+    }
+
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
         while (_) try {
             if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
             if (y = 0, t) op = [op[0] & 2, t.value];
             switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                case 0:
+                case 1:
+                    t = op;
+                    break;
+                case 4:
+                    _.label++;
+                    return {
+                        value: op[1], done: false
+                    };
+                case 5:
+                    _.label++;
+                    y = op[1];
+                    op = [0];
+                    continue;
+                case 7:
+                    op = _.ops.pop();
+                    _.trys.pop();
+                    continue;
                 default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) {
+                        _ = 0;
+                        continue;
+                    }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) {
+                        _.label = op[1];
+                        break;
+                    }
+                    if (op[0] === 6 && _.label < t[1]) {
+                        _.label = t[1];
+                        t = op;
+                        break;
+                    }
+                    if (t && _.label < t[2]) {
+                        _.label = t[2];
+                        _.ops.push(op);
+                        break;
+                    }
                     if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
+                    _.trys.pop();
+                    continue;
             }
             op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+        } catch (e) {
+            op = [6, e];
+            y = 0;
+        } finally {
+            f = t = 0;
+        }
+        if (op[0] & 5) throw op[1];
+        return {
+            value: op[0] ? op[1] : void 0,
+            done: true
+        };
     }
 };
 exports.__esModule = true;
 require("dotenv/config");
 var ring_client_api_1 = require("ring-client-api");
 var util_1 = require("util");
-var fs = require('fs'), path = require('path'), http = require('http'), url = require('url'), zlib = require('zlib');
+var fs = require('fs'),
+    path = require('path'),
+    http = require('http'),
+    url = require('url'),
+    zlib = require('zlib');
 //const PORT = 3000;
 var PORT = process.env.RING_PORT;
 /**
@@ -60,12 +143,12 @@ function startStream() {
                         refreshToken: process.env.RING_REFRESH_TOKEN,
                         debug: true
                     });
-                    return [4 /*yield*/, ringApi.getCameras()];
+                    return [4 /*yield*/ , ringApi.getCameras()];
                 case 1:
                     camera = (_a.sent())[0];
                     if (!camera) {
                         console.log('No cameras found');
-                        return [2 /*return*/];
+                        return [2 /*return*/ ];
                     }
                     publicOutputDirectory = path.join('public/');
                     /*fs.readdir(publicOutputDirectory, (err, files) => {
@@ -84,7 +167,9 @@ function startStream() {
                         var uri = url.parse(req.url).pathname;
                         console.log('requested uri: ' + uri);
                         if (uri == '/index.html' || uri == '/') {
-                            res.writeHead(200, { 'Content-Type': 'text/html' });
+                            res.writeHead(200, {
+                                'Content-Type': 'text/html'
+                            });
                             res.write('<html><head><title>Ring Livestream' +
                                 '</title></head><body>');
                             res.write('<h1>Welcome to your Ring Livestream!</h1>');
@@ -98,11 +183,12 @@ function startStream() {
                         fs.exists(filename, function (exists) {
                             if (!exists) {
                                 console.log('file not found: ' + filename);
-                                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                                res.writeHead(404, {
+                                    'Content-Type': 'text/plain'
+                                });
                                 res.write('file not found: %s\n', filename);
                                 res.end();
-                            }
-                            else {
+                            } else {
                                 console.log('sending file: ' + filename);
                                 switch (path.extname(uri)) {
                                     case '.m3u8':
@@ -110,23 +196,24 @@ function startStream() {
                                             if (err) {
                                                 res.writeHead(500);
                                                 res.end();
-                                            }
-                                            else if (contents) {
-                                                res.writeHead(200, { 'Content-Type': 'application/vnd.apple.mpegurl' });
+                                            } else if (contents) {
+                                                res.writeHead(200, {
+                                                    'Content-Type': 'application/vnd.apple.mpegurl'
+                                                });
                                                 var ae = req.headers['accept-encoding'];
                                                 if (ae && ae.match(/\bgzip\b/)) {
                                                     zlib.gzip(contents, function (err, zip) {
                                                         if (err)
                                                             throw err;
-                                                        res.writeHead(200, { 'content-encoding': 'gzip' });
+                                                        res.writeHead(200, {
+                                                            'content-encoding': 'gzip'
+                                                        });
                                                         res.end(zip);
                                                     });
-                                                }
-                                                else {
+                                                } else {
                                                     res.end(contents, 'utf-8');
                                                 }
-                                            }
-                                            else {
+                                            } else {
                                                 console.log('empty playlist');
                                                 res.writeHead(500);
                                                 res.end();
@@ -134,8 +221,12 @@ function startStream() {
                                         });
                                         break;
                                     case '.ts':
-                                        res.writeHead(200, { 'Content-Type': 'video/MP2T' });
-                                        var stream = fs.createReadStream(filename, { bufferSize: 64 * 1024 });
+                                        res.writeHead(200, {
+                                            'Content-Type': 'video/MP2T'
+                                        });
+                                        var stream = fs.createReadStream(filename, {
+                                            bufferSize: 64 * 1024
+                                        });
                                         stream.pipe(res);
                                         break;
                                     default:
@@ -162,14 +253,15 @@ function startStream() {
                         socket.setTimeout(4000);
                     });
                     console.log('Started server, listening on port ' + PORT + '.');
-                    return [4 /*yield*/, util_1.promisify(fs.exists)(publicOutputDirectory)];
+                    return [4 /*yield*/ , util_1.promisify(fs.exists)(publicOutputDirectory)];
                 case 2:
-                    if (!!(_a.sent())) return [3 /*break*/, 4];
-                    return [4 /*yield*/, util_1.promisify(fs.mkdir)(publicOutputDirectory)];
+                    if (!!(_a.sent())) return [3 /*break*/ , 4];
+                    return [4 /*yield*/ , util_1.promisify(fs.mkdir)(publicOutputDirectory)];
                 case 3:
                     _a.sent();
                     _a.label = 4;
-                case 4: return [4 /*yield*/, camera.streamVideo({
+                case 4:
+                    return [4 /*yield*/ , camera.streamVideo({
                         output: [
                             '-preset',
                             'veryfast',
@@ -192,7 +284,9 @@ function startStream() {
                     sipSession = _a.sent();
                     sipSession.onCallEnded.subscribe(function () {
                         console.log('Call has ended');
-                        server.close(function () { console.log('Server closed!'); });
+                        server.close(function () {
+                            console.log('Server closed!');
+                        });
                         // Destroy all open sockets
                         for (var socketId in sockets) {
                             console.log('socket', socketId, 'destroyed');
@@ -206,7 +300,7 @@ function startStream() {
                         console.log('Stopping call...');
                         sipSession.stop();
                     }, 10 * 60 * 1000); // 10*60*1000 Stop after 10 minutes.
-                    return [2 /*return*/];
+                    return [2 /*return*/ ];
             }
         });
     });
@@ -214,7 +308,9 @@ function startStream() {
 if (!('RING_EMAIL' in process.env) || !('RING_PASS' in process.env) || !('RING_PORT' in process.env)) {
     console.log('Missing environment variables. Check RING_EMAIL, RING_PASS and RING_PORT are set.');
     process.exit();
-}
-else {
+} else {
+    console.log("process.env")
+    console.log(process.env)
+    console.log(process.env.RING_REFRESH_TOKEN)
     startStream();
 }
