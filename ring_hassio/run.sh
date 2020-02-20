febe87205616ab782b944a4e153fed61fb5aec04
Riegel, Lukas <lukas.riegel@sap.com>
2020-02-20 09:11:38 +0100

    2FA

--- ring_hassio/run.sh
+++ ring_hassio/run.sh
@@ -2,6 +2,7 @@ CONFIG_PATH=/data/options.json
 export RING_PORT="$(jq --raw-output '.port' $CONFIG_PATH)"
 export RING_EMAIL="$(jq --raw-output '.ring_username' $CONFIG_PATH)"
 export RING_PASS="$(jq --raw-output '.ring_password' $CONFIG_PATH)"
+export RING_REFRESH_TOKEN="$(jq --raw-output '.ring_refresh_token' $CONFIG_PATH)"
 
 cd /ring-hassio/ring_hassio
 node livestream.js
